fba38d6eb8aa490e1045617926dc1e89b27fbe96
Eymard Cantu <210300536@ucaribe.edu.mx>
2024-11-26 22:21:20 -0500

    actualización del auth routes

--- api/v1/routes/auth.py
+++ api/v1/routes/auth.py
@@ -1,15 +1,17 @@
-from fastapi import APIRouter, HTTPException, Depends
-from sqlalchemy.orm import Session
-from schemas.auth import Token
-from core.config import get_db
+from fastapi import APIRouter, HTTPException
+from schemas.auth import Token, LoginRequest
 from services.auth import create_access_token
 
 router = APIRouter()
 
-@router.post("/login", response_model=Token)
-async def login(username: str, password: str, db: Session = Depends(get_db)):
-    if username != "admin" or password != "admin":
+@router.post("/login", 
+             tags=["Login"],
+             response_model=Token,
+             description="Get a list of all students")
+async def login(request: LoginRequest):
+    # Aquí puedes validar usuario y contraseña (mock por ahora)
+    if request.username != "admin" or request.password != "admin":
         raise HTTPException(status_code=401, detail="Invalid credentials")
     
-    access_token = create_access_token({"sub": username})
-    return {"access_token": access_token, "token_type": "bearer"}
+    access_token = create_access_token({"sub": request.username})
+    return {"access_token": access_token, "token_type": "bearer"}
