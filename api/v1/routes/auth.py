e3007aa17cfe59445f43bf9f533750ff16c210d7
Eymard Cantu <210300536@ucaribe.edu.mx>
2024-11-26 22:23:45 -0500

    actualización del auth routes

--- api/v1/routes/auth.py
+++ api/v1/routes/auth.py
@@ -9,7 +9,6 @@ router = APIRouter()
              response_model=Token,
              description="Get a list of all students")
 async def login(request: LoginRequest):
-    # Aquí puedes validar usuario y contraseña (mock por ahora)
     if request.username != "admin" or request.password != "admin":
         raise HTTPException(status_code=401, detail="Invalid credentials")
     
